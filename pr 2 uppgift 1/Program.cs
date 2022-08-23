9abc0f6b1cd228a4ec980f205e2fd7230257ddd8
Josund04 <89245272+Josund04@users.noreply.github.com>
2022-08-23 11:23:34 +0200

    Add files via upload

---
+++ pr 2 uppgift 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class Car {
+    public string tillverkare;
+    public int modell;
+    public int årtal;
+    public int vikt;
+}
+
