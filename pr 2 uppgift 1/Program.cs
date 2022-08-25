1daae3e88ba8635cef2a00da3984b3677e696d47
EmilWallett <nhlpool27@gmail.com>
2022-08-25 12:24:43 +0200

    2 klar

--- pr 2 uppgift 1/Program.cs
+++ pr 2 uppgift 1/Program.cs
@@ -1,9 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Car m = new Car();
+        m.tillverkare = "Mazda";
+        m.modell = "2";
+        m.årtal = 2003;
+        m.vikt = 1170;
+       
+        Console.WriteLine(m.tillverkare);
+        Console.WriteLine(m.modell);
+        Console.WriteLine(m.årtal);
+        Console.WriteLine(m.vikt);
+
+        Car v = new Car();
+        v.tillverkare = "Volvo";
+        v.modell = "v90";
+        v.årtal = 2017;
+        v.vikt = 1879;
+       
+        Console.WriteLine(v.tillverkare);
+        Console.WriteLine(v.modell);
+        Console.WriteLine(v.årtal);
+        Console.WriteLine(v.vikt);
 class Car {
+    
     public string tillverkare;
-    public int modell;
+    public string modell;
     public int årtal;
     public int vikt;
 }
-
